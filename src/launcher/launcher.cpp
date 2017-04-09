87809da1343d5702ec741bffb9707d1a268ca88c
Sam <duo929@hotmail.com>
2017-04-09 15:07:51 +0200

    Add main CMakeLists file, add launcher CMakeLists file, add launcher.cpp with empty WinMain

---
+++ src/launcher/launcher.cpp
@@ -0,0 +1,12 @@
+#define WIN32_LEAN_AND_MEAN
+#include <Windows.h>
+
+int CALLBACK WinMain(
+	HINSTANCE hInstance,
+	HINSTANCE hPrevInstance,
+	LPSTR     lpCmdLine,
+	int       nCmdShow
+)
+{
+	return 0;
+}
