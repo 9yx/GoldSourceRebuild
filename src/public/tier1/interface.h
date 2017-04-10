016942641ab2c0d7d7f4bdc887fe12d7a92e758a
Sam <duo929@hotmail.com>
2017-04-10 15:53:23 +0200

    Add tier1 public & private files

---
+++ src/public/tier1/interface.h
@@ -0,0 +1,19 @@
+//========= Copyright © 1996-2005, Valve Corporation, All rights reserved. ============//
+//
+// Purpose: 
+//
+// $NoKeywords: $
+//
+//=============================================================================//
+
+#ifndef TIER1_INTERFACE_H
+#define TIER1_INTERFACE_H
+
+#include "tier0/platform.h"
+
+#include "public/interface.h"
+
+#endif
+
+
+
