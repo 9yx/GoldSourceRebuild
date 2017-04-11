3f71de727655fab4d842af1d1d080f7586e9690a
Sam <duo929@hotmail.com>
2017-04-11 18:48:44 +0200

    Implement COM_CheckParm, COM_InitArgv, COM_FileBase, COM_SetupDirectories, COM_ParseDirectoryFromCmd, COM_FixSlashes

--- src/engine/common.cpp
+++ src/engine/common.cpp
@@ -22,6 +22,211 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #include <cstdio>
 
 #include "common.h"
+#include "filesystem.h"
+#include "info.h"
+#include "quakedef.h"
+#include "strtools.h"
+
+#define NUM_SAFE_ARGVS  7
+
+static const char* largv[ MAX_NUM_ARGVS + NUM_SAFE_ARGVS + 1 ];
+static const char* argvdummy = " ";
+
+static const char* safeargvs[ NUM_SAFE_ARGVS ] =
+{ "-stdvid", "-nolan", "-nosound", "-nocdaudio", "-nojoy", "-nomouse", "-dibonly" };
+
+int		com_argc = 0;
+const char** com_argv = nullptr;
+
+#define CMDLINE_LENGTH	256
+char	com_cmdline[ CMDLINE_LENGTH ];
+
+char com_clientfallback[ FILENAME_MAX ] = {};
+char com_gamedir[ FILENAME_MAX ] = {};
+
+int COM_CheckParm( const char* parm )
+{
+	for( int i = 1; i<com_argc; i++ )
+	{
+		if( !com_argv[ i ] )
+			continue;               // NEXTSTEP sometimes clears appkit vars.
+		if( !Q_strcmp( parm, com_argv[ i ] ) )
+			return i;
+	}
+
+	return 0;
+}
+
+void COM_InitArgv( int argc, const char** argv )
+{
+	int             i, n;
+
+	// reconstitute the command line for the cmdline externally visible cvar
+	n = 0;
+
+	for( int j = 0; ( j<MAX_NUM_ARGVS ) && ( j< argc ); j++ )
+	{
+		i = 0;
+
+		while( ( n < ( CMDLINE_LENGTH - 1 ) ) && argv[ j ][ i ] )
+		{
+			com_cmdline[ n++ ] = argv[ j ][ i++ ];
+		}
+
+		if( n < ( CMDLINE_LENGTH - 1 ) )
+			com_cmdline[ n++ ] = ' ';
+		else
+			break;
+	}
+
+	com_cmdline[ n ] = 0;
+
+	bool safe = false;
+
+	for( com_argc = 0; ( com_argc<MAX_NUM_ARGVS ) && ( com_argc < argc );
+		 com_argc++ )
+	{
+		largv[ com_argc ] = argv[ com_argc ];
+		if( !Q_strcmp( "-safe", argv[ com_argc ] ) )
+			safe = true;
+	}
+
+	if( safe )
+	{
+		// force all the safe-mode switches. Note that we reserved extra space in
+		// case we need to add these, so we don't need an overflow check
+		for( i = 0; i<NUM_SAFE_ARGVS; i++ )
+		{
+			largv[ com_argc ] = safeargvs[ i ];
+			com_argc++;
+		}
+	}
+
+	largv[ com_argc ] = argvdummy;
+	com_argv = largv;
+}
+
+void COM_FileBase( const char *in, char *out )
+{
+	if( !in )
+		return;
+
+	if( !*in )
+		return;
+
+	size_t uiLength = strlen( in );
+
+	// scan backward for '.'
+	size_t end = uiLength - 1;
+	while( end && in[ end ] != '.' && in[ end ] != '/' && in[ end ] != '\\' )
+		--end;
+
+	if( in[ end ] != '.' )	// no '.', copy to end
+		end = uiLength - 1;
+	else
+		--end;				// Found ',', copy to left of '.'
+
+
+	// Scan backward for '/'
+	size_t start = uiLength;
+	while( start > 0 && in[ start - 1 ] != '/' && in[ start - 1 ] != '\\' )
+		--start;
+
+	// Length of new string
+	uiLength = end - start + 1;
+
+	// Copy partial string
+	strncpy( out, &in[ start ], uiLength );
+	// Terminate it
+	out[ uiLength ] = '\0';
+}
+
+bool COM_SetupDirectories()
+{
+	com_clientfallback[ 0 ] = '\0';
+	com_gamedir[ 0 ] = '\0';
+
+	char basedir[ 512 ];
+
+	COM_ParseDirectoryFromCmd( "-basedir", basedir, "valve" );
+	COM_ParseDirectoryFromCmd( "-game", com_gamedir, basedir );
+
+	bool bResult = FileSystem_SetGameDirectory( basedir, com_gamedir[ 0 ] ? com_gamedir : nullptr );
+
+	if( bResult )
+	{
+		//TODO: serverinfo is only 256 characters large, but 512 is passed in. - Solokiller
+		Info_SetValueForStarKey( serverinfo, "*gamedir", com_gamedir, 512 );
+	}
+
+	return bResult;
+}
+
+void COM_ParseDirectoryFromCmd( const char *pCmdName, char *pDirName, const char *pDefault )
+{
+	const char* pszResult = nullptr;
+
+	if( com_argc > 1 )
+	{
+		for( int arg = 1; arg < com_argc; ++arg )
+		{
+			auto pszArg = com_argv[ arg ];
+
+			if( pszArg )
+			{
+				if( pCmdName && *pCmdName == *pszArg )
+				{
+					if( *pCmdName && !strcmp( pCmdName, pszArg ) )
+					{
+						if( arg < com_argc - 1 )
+						{
+							auto pszValue = com_argv[ arg + 1 ];
+
+							if( *pszValue != '+' && *pszValue != '-' )
+							{
+								strcpy( pDirName, pszValue );
+								pszResult = pDirName;
+								break;
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+
+	if( !pszResult )
+	{
+		if( pDefault )
+		{
+			strcpy( pDirName, pDefault );
+			pszResult = pDirName;
+		}
+		else
+		{
+			pszResult = pDirName;
+			*pDirName = '\0';
+		}
+	}
+
+	const auto uiLength = strlen( pszResult );
+
+	if( uiLength > 0 )
+	{
+		auto pszEnd = &pDirName[ uiLength - 1 ];
+		if( *pszEnd == '/' || *pszEnd == '\\' )
+			*pszEnd = '\0';
+	}
+}
+
+void COM_FixSlashes( char *pname )
+{
+	for( char* pszNext = pname; *pszNext; ++pszNext )
+	{
+		if( *pszNext == '\\' )
+			*pszNext = '/';
+	}
+}
 
 char* va( const char* format, ... )
 {
--- src/engine/common.h
+++ src/engine/common.h
@@ -20,7 +20,28 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #ifndef ENGINE_COMMON_H
 #define ENGINE_COMMON_H
 
+#include <cstdio>
+
 // comndef.h  -- general definitions
+
+extern int com_argc;
+extern const char** com_argv;
+extern char com_gamedir[ FILENAME_MAX ];
+
+/**
+*	Returns the position (1 to argc-1) in the program's argument list
+*	where the given parameter apears, or 0 if not present
+*/
+int COM_CheckParm( const char* parm );
+void COM_InitArgv( int argc, const char** argv );
+
+void COM_FileBase( const char *in, char *out );
+
+bool COM_SetupDirectories();
+
+void COM_ParseDirectoryFromCmd( const char *pCmdName, char *pDirName, const char *pDefault );
+
+void COM_FixSlashes( char *pname );
 
 /**
 *	does a varargs printf into a temp buffer, so I don't need to have
