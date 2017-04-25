c95d0db7ccf0c21052748cab75661dbbd3d9421c
Sam <duo929@hotmail.com>
2017-04-25 16:57:02 +0200

    Fix forward declaration of characterset_t struct

--- src/public/FileSystem_Helpers.h
+++ src/public/FileSystem_Helpers.h
@@ -14,7 +14,7 @@
 class IFileSystem;
 
 // Call until it returns NULL. Each time you call it, it will parse out a token.
-typedef struct characterset_s characterset_t;
+struct characterset_t;
 const char* ParseFile( const char* pFileBytes, char* pToken, bool* pWasQuoted, characterset_t *pCharSet = NULL );
 char* ParseFile( char* pFileBytes, char* pToken, bool* pWasQuoted );	// (same exact thing as the const version)
 
