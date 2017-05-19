d72ddae5516c7d6a0f11bd16915a126e3c9bdb27
Sam <duo929@hotmail.com>
2017-05-19 20:57:36 +0200

    Add partial implementation of GameUI Shared menu code mostly implemented missing Demo UI, Career UI, console is broken

--- src/common/GameUI/CareerDefs.h
+++ src/common/GameUI/CareerDefs.h
@@ -10,19 +10,46 @@ enum CareerStateType
 	CAREER_PLAYING
 };
 
-//TODO: define - Solokiller
 enum CareerDifficultyType
 {
+	CAREER_DIFFICULTY_EASY,
+	CAREER_DIFFICULTY_NORMAL,
+	CAREER_DIFFICULTY_HARD,
+	CAREER_DIFFICULTY_EXPERT,
+	MAX_CAREER_DIFFICULTY
 };
 
-//TODO: define - Solokiller
 struct MapInfo
 {
-
+	const char* name;
+	bool defeated;
 };
 
-//TODO: define - Solokiller
-class ICareerTask;
+class ICareerTask
+{
+public:
+	virtual bool IsComplete() = 0;
+	virtual bool IsCompletedThisRound() = 0;
+	virtual bool IsCompletedThisMatch() = 0;
+
+	virtual const char* GetMap() = 0;
+
+	virtual const char* GetTaskName() = 0;
+	virtual const wchar_t* GetLocalizedTaskName() = 0;
+	virtual const wchar_t* GetLocalizedTaskNameWithCompletion() = 0;
+
+	virtual void Reset() = 0;
+	virtual void Set( bool thisRound ) = 0;
+	virtual void StartRound() = 0;
+
+	virtual const char* GetWeaponName() const = 0;
+	virtual int GetRepeat() const = 0;
+	virtual bool MustSurvive() const = 0;
+	virtual bool InARow() const = 0;
+	virtual int GetPartial() const = 0;
+	virtual void SetPartial( int num ) = 0;
+	virtual bool IsMaxRoundTime() const = 0;
+};
 
 typedef std::vector<ICareerTask*> ITaskVec;
 
