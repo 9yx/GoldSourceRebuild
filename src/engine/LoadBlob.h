61509491aa7ed39ad92d1b93b593218ee6b71a96
Sam <duo929@hotmail.com>
2017-05-28 23:22:20 +0200

    Implement blob loading code

---
+++ src/engine/LoadBlob.h
@@ -0,0 +1,19 @@
+#ifndef ENGINE_LOADBLOB_H
+#define ENGINE_LOADBLOB_H
+
+#include <tier0/platform.h>
+
+struct BlobFootprint_t
+{
+	int32 m_hDll;
+};
+
+using FFunction = void ( * )( void* );
+
+int FIsBlob( const char* pstFileName );
+
+void FreeBlob( BlobFootprint_t* pblobfootprint );
+
+void* NLoadBlobFile( const char* pstFileName, BlobFootprint_t* pblobfootprint, void* pv, char fLoadAsDll );
+
+#endif //ENGINE_LOADBLOB_H
